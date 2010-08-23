0e359743039946d748c7c9660460ba39b7181051
Hidden <zzdhidden@gmail.com>
2010-08-23 13:05:51 +0800

    Fix

--- api/online.php
+++ api/online.php
@@ -129,7 +129,7 @@ $output['user']=array('id'=>$space['username'],
                        'status_time'=>'',
                        'show '=>'chat',
                        'url'=>'space.php?uid='.$space['uid']);//用户信息
-$imserver = 'http://'.$_IMC['imsvr'].':'.$_IMC['impoll'];
+$imserver = 'http://'.$_IMC['imsvr'].':'.$_IMC['impoll']."/packets";
 $output['connection'] = array('domain' => $_IMC['domain'], 'ticket'=>$ticket, 'server'=>$imserver);//服务器连接
 
 $output['new_messages'] = $new_messages;
--- custom.js.php
+++ custom.js.php
@@ -1,35 +1,16 @@
 <?php
 header("Content-type: application/javascript");
 include_once 'config.php';
-$platform = 'uchome';
+include_once('lib/uchome.php');
+$menu = array(
+	array("title" => 'doing',"icon" =>"image/app/doing.gif","link" => "space.php?do=doing"),
+	array("title" => 'album',"icon" =>"image/app/album.gif","link" => "space.php?do=album"),
+	array("title" => 'blog',"icon" =>"image/app/blog.gif","link" => "space.php?do=blog"),
+	array("title" => 'thread',"icon" =>"image/app/mtag.gif","link" => "space.php?do=thread"),
+	array("title" => 'share',"icon" =>"image/app/share.gif","link" => "space.php?do=share")
+);
 
-switch($platform){
-	case 'discuz':
-		include_once('lib/discuz.php');
-		break;
-	case 'uchome':
-		include_once('lib/uchome.php');
-		break;
-}
-
-if($platform === 'uchome'){
-	$menu = array(
-		array("title" => 'doing',"icon" =>"image/app/doing.gif","link" => "space.php?do=doing"),
-		array("title" => 'album',"icon" =>"image/app/album.gif","link" => "space.php?do=album"),
-		array("title" => 'blog',"icon" =>"image/app/blog.gif","link" => "space.php?do=blog"),
-		array("title" => 'thread',"icon" =>"image/app/mtag.gif","link" => "space.php?do=thread"),
-		array("title" => 'share',"icon" =>"image/app/share.gif","link" => "space.php?do=share")
-	);
-}else if($platform === 'discuz'){
-	$menu = array(
-		array("title" => 'search',"icon" =>"webim/static/images/search.png","link" => "search.php"),
-		array("title" => 'faq',"icon" =>"webim/static/images/faq.png","link" => "faq.php"),
-		array("title" => 'nav',"icon" =>"webim/static/images/nav.png","link" => "misc.php?action=nav"),
-		array("title" => 'feeds',"icon" =>"webim/static/images/feeds.png","link" => "index.php?op=feeds"),
-		array("title" => 'sms',"icon" =>"webim/static/images/msm.png","link" => "pm.php")
-	);
-}
-$menu[] = array("title" => 'imlogo',"icon" =>"webim/static/images/nextim.gif","link" => "http://www.nextim.cn");
+//$menu[] = array("title" => 'imlogo',"icon" =>"webim/static/images/nextim.gif","link" => "http://www.nextim.cn");
 if($_SCONFIG['my_status']) {
 	if(is_array($_SGLOBAL['userapp'])) { 
 		foreach($_SGLOBAL['userapp'] as $value) { 
@@ -40,35 +21,33 @@ if($_SCONFIG['my_status']) {
 $setting = json_encode(setting());
 
 ?>
-
 //custom
 (function(webim){
     var path = "";
-    var platform = "<?php echo $platform ?>";
 
     var menu = webim.JSON.decode('<?php echo json_encode($menu) ?>');
 	webim.extend(webim.setting.defaults.data, webim.JSON.decode('<?php echo $setting ?>'));
 	var webim = window.webim;
 	webim.defaults.urls = {
-		online:path + "webim/api/online.php?platform=" + platform,
-		offline:path + "webim/api/offline.php?platform=" + platform,
-		message:path + "webim/api/message.php?platform=" + platform,
-		presence:path + "webim/api/presence.php?platform=" + platform,
-		refresh:path + "webim/api/refresh.php?platform=" + platform,
-		status:path + "webim/api/status.php?platform=" + platform
+		online:path + "webim/api/online.php",
+		offline:path + "webim/api/offline.php",
+		message:path + "webim/api/message.php",
+		presence:path + "webim/api/presence.php",
+		refresh:path + "webim/api/refresh.php",
+		status:path + "webim/api/status.php"
 	};
-	webim.setting.defaults.url = path + "webim/api/setting.php?platform="+platform;
+	webim.setting.defaults.url = path + "webim/api/setting.php";
 	webim.history.defaults.urls = {
-		load: path + "webim/api/history.php?platform=" + platform,
-		clear: path + "webim/api/clear_history.php?platform=" + platform
+		load: path + "webim/api/history.php",
+		clear: path + "webim/api/clear_history.php"
 	};
     	webim.room.defaults.urls = {
-                    member: path + "webim/api/members.php?platform=" + platform,
-                    join: path + "webim/api/join.php?platform=" + platform,
-                    leave: path + "webim/api/leave.php?platform=" + platform
+                    member: path + "webim/api/members.php",
+                    join: path + "webim/api/join.php",
+                    leave: path + "webim/api/leave.php"
     	};
-	webim.buddy.defaults.url = path + "webim/api/user_info.php?platform=" + platform;
-	webim.notification.defaults.url = path + "webim/api/notifications.php?platform=" + platform;
+	webim.buddy.defaults.url = path + "webim/api/user_info.php";
+	webim.notification.defaults.url = path + "webim/api/notifications.php";
     
 
 	webim.ui.emot.init({"dir": path + "webim/static/images/emot/default"});
