bde4912eb30743a3870531aff5253d3b45fb300e
kehao <kehao.qiu@gmail.com>
2010-08-28 22:18:35 +0800

    online

--- online.php
+++ online.php
@@ -1,7 +1,8 @@
 <?php
 include_once('common.php');
 $friend_ids = ids_array($space['friends']);
-$buddy_ids = ids_array(gp("buddy_ids"));//正在聊天的联系人
+//$buddy_ids = ids_array(gp("buddy_ids"));//正在聊天的联系人
+//$buddy_ids = ids_array("orez88,admin");//正在聊天的联系人
 
 $new_messages = find_new_message();//查找离线消息
 for($i=0;$i<count($new_messages);$i++) {
@@ -24,14 +25,15 @@ if(!empty($friend_ids)) {
     $ids=join(",",$friend_ids);
     $query = $_SGLOBAL['db']-> query("SELECT username FROM ".tname('space')." WHERE uid IN ($ids)");
     while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-        $buddie_ids[] = $value['username'];
+        $buddy_ids[] = $value['username'];
 
     }
 }
-$buddie_ids=array_unique($buddie_ids);
+
+$buddy_ids=array_unique($buddy_ids);
 
 $im = new WebIM($user, null, $_IMC['domain'], $_IMC['apikey'], $_IMC['host'], $_IMC['port']);
-$data = $im->online(implode(",",$buddie_ids), implode(",", $room_ids));
+$data = $im->online(implode(",",$buddy_ids), implode(",", $room_ids));
 if($data->success) {
     $_rooms=array();
     //Add room online member count.
@@ -40,10 +42,36 @@ if($data->success) {
         $rooms[$id]['count'] = $v->count;
         $_rooms[]=$rooms[$id];
     }
-  
+
+  $buddylist=array();
+  $_buddies=buddy($buddy_ids);
+
+  if(!empty($_buddies)) {
+      foreach($_buddies as $buddy) {
+          $buddy['show']="unavailable";
+          $buddy['need_reload']=false;
+          $buddy['presence']="offline";
+          foreach($data->buddies as $online_buddy) {
+              if(!(in_array($online_buddy->id,$buddy_ids))) {
+                  $buddylist[]=$online_buddy;
+              }
+              if($buddy['id']==$online_buddy->id) {
+                 $buddy['show']=$online_buddy->show;
+                 $buddy['need_reload']=$online_buddy->need_reload;
+                 $buddy['presence']=$online_buddy->presence;
+              }
+
+          }
+          $buddylist[]=$buddy;
+      }
+  }
+  else {
+      $buddylist=(array)$data->buddies;
+  }
+
     $data->rooms = $_rooms;
-    $online_buddies=build_buddies($data->buddies);
-    $data->buddies=array_merge($online_buddies,buddy($buddy_ids));
+   
+    $data->buddies=$buddylist;
     $data->histories=find_history($buddy_ids);
     $data->new_messages=$new_messages;
     echo json_encode($data);
