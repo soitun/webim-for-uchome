84e447dc233f7348b0080074e5efbef717a2a9ea
Hidden <zzdhidden@gmail.com>
2010-09-10 10:20:36 +0800

    Notice config

--- check.php
+++ check.php
@@ -1,6 +1,5 @@
 <?php
 header("Content-type: application/javascript");
-include_once('common.php');
 
 echo "Connnect webim.us\n\n\n";
 echo file_get_contents("http://www.webim.us/robots.txt");
@@ -8,7 +7,12 @@ echo file_get_contents("http://www.webim.us/robots.txt");
 echo "\n\n\nConnnect webim20.cn\n\n\n";
 echo file_get_contents("http://www.webim20.cn/robots.txt");
 
+include_once('common.php');
 $im = new WebIM($user, null, $_IMC['domain'], $_IMC['apikey'], $_IMC['host'], $_IMC['port']);
 
-echo "\n\n\nIM Connnect... \n\n\n";
+echo "\n\n\nIM Connnect... \n";
+echo "'" . $_IMC['domain'] . "'\n";
+echo "'" . $_IMC['host'] . "'\n";
+echo "'" . $_IMC['port'] . "'\n\n\n";
+
 print_r($im->check_connect());
