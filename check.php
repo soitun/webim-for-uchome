5093b46eff167a0dffdde3f987c61be9fb5d08f9
Hidden <zzdhidden@gmail.com>
2010-09-10 10:14:15 +0800

    Check webim connect

--- check.php
+++ check.php
@@ -1,8 +1,13 @@
 <?php
 header("Content-type: application/javascript");
+include_once('common.php');
 
 echo "Connnect webim.us\n\n\n";
 echo file_get_contents("http://www.webim.us/robots.txt");
 
 echo "\n\n\nConnnect webim20.cn\n\n\n";
 echo file_get_contents("http://www.webim20.cn/robots.txt");
+
+$im = new WebIM($user, null, $_IMC['domain'], $_IMC['apikey'], $_IMC['host'], $_IMC['port']);
+
+print_r($im->check_connect());
