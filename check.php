0dca38464cc8b55df72310903a6f27e631e3f125
Hidden <zzdhidden@gmail.com>
2010-09-10 13:27:53 +0800

    Check

--- check.php
+++ check.php
@@ -1,18 +1,18 @@
 <?php
 header("Content-type: application/javascript");
 
-echo "Connnect webim.us\n\n\n";
-echo file_get_contents("http://www.webim.us/robots.txt");
-
-echo "\n\n\nConnnect webim20.cn\n\n\n";
-echo file_get_contents("http://www.webim20.cn/robots.txt");
-
 include_once('common.php');
 $im = new WebIM($user, null, $_IMC['domain'], $_IMC['apikey'], $_IMC['host'], $_IMC['port']);
 
-echo "\n\n\nIM Connnect... \n";
+echo "\n\nIM Config \n";
 echo "'" . $_IMC['domain'] . "'\n";
 echo "'" . $_IMC['host'] . "'\n";
-echo "'" . $_IMC['port'] . "'\n\n\n";
+echo "'" . $_IMC['port'] . "'\n";
 
+echo "\n\n\nIM check online \n";
+$client = new HttpClient(trim($_IMC['host']), trim($_IMC['port']));
 print_r($im->check_connect());
+
+echo "Test for port 80\n";
+echo file_get_contents("http://www.webim20.cn");
+
