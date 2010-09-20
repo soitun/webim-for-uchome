8c3697327b5e3b4860c7688d7295a5ac7e8106be
Hidden <zzdhidden@gmail.com>
2010-09-20 20:12:28 +0800

    Switch to regular version

--- common.php
+++ common.php
@@ -1,275 +1,69 @@
 <?php
-require_once('lib/webim.class.php');
-require_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'common.php');
-require_once("lib/json.php");
-
-$is_login = false;
-if(empty($_SGLOBAL['supe_uid'])) {
-	$is_login = false;
-} else {
-	$is_login = true;
-	$space = getspace($_SGLOBAL['supe_uid']);
-}
-if(!$is_login)exit('Login at first.');
-$user->uid = $space['uid'];
-$user->id = to_utf8($space['username']);
-$user->nick = to_utf8(nick($space));
-$user->pic_url = avatar($user->uid,"small",true);
-$user->default_pic_url=UC_API.'/images/noavatar_small.gif';
-$user->show = gp('show') ? gp('show') : "available";
-$user->url = "space.php?uid=".$user->uid;
-
-$_SGLOBAL['db']->query("SET NAMES utf8");
-$groups = getfriendgroup();
-foreach($groups as $k => $v){
-	$groups[$k] = to_utf8($v);
-}
-
-#connect ucenter db.
-include_once(S_ROOT.'./source/class_mysql.php');
-$ucdb = new dbstuff;
-$ucdb->charset = UC_DBCHARSET;
-$ucdb->connect(UC_DBHOST, UC_DBUSER, UC_DBPW, UC_DBNAME);
-$ucdb->query("SET NAMES utf8");
-
-
-function nick($sp) {
-	global $_IMC;
-	return (!$_IMC['show_realname']||empty($sp['name'])) ? $sp['username'] : $sp['name'];
-}
-
-function ids_array($ids) {
-	return ($ids===NULL || $ids==="") ? array() : (is_array($ids) ? array_unique($ids) : array_unique(split(",", $ids)));
-}
-function ids_except($id, $ids) {
-	if(in_array($id, $ids)) {
-		array_splice($ids, array_search($id, $ids), 1);
-	}
-	return $ids;
-}
-
-function im_tname($name) {
-	//     return "`webim_".$name."`";
-	return UC_DBTABLEPRE."webim_".$name;
-}
-
-
-function online_buddy(){
-	global $groups, $user, $_SGLOBAL;
-	$list = array();
-	$query = $_SGLOBAL['db']->query("SELECT m.uid, m.username, m.name, f.gid 
-		FROM ".tname('friend')." f, ".tname('session')." s, ".tname('space')." m
-		WHERE f.uid='$user->uid' AND f.fuid = s.uid AND m.uid = s.uid
-		ORDER BY f.num DESC, f.dateline DESC");
-	while ($value = $_SGLOBAL['db']->fetch_array($query)){
-		$list[] = (object)array(
-			"uid" => $value['uid'],
-			"id" => $value['username'],
-			"nick" => nick($value),
-			"group" => $groups[$value['gid']],
-			"url" => "home.php?mod=space&uid=".$value['uid'],
-			'default_pic_url' => UC_API.'/images/noavatar_small.gif',
-			"pic_url" => avatar($value['uid'], 'small', true),
-		);
-	}
-	return $list;
-}
-
-
-function complete_status($members){
-	global $_SGLOBAL;
-	if(!empty($members)){                
-		$num = count($members);                
-		$ids = array();
-		$ob = array();
-		for($i = 0; $i < $num; $i++){
-			$m = $members[$i];
-			$id = $m->uid;
-			if ( $id ) {
-				$ids[] = $id;
-				$ob[$id] = $m;
-			}
-		}
-		$ids = implode(",", $ids);
-		$query = $_SGLOBAL['db']-> query($q="SELECT t.uid, t.message FROM " . tname("doing") . " t 
-			INNER JOIN (SELECT max(doid) doid 
-			FROM " . tname("doing") . "  
-			WHERE uid IN ($ids)
-			GROUP BY uid) t2 
-			ON t2.doid = t.doid;");
-		while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-			$ob[$value['uid']]->status = $value['message'];
-		}
-	}
-	return $members;
-}
-
-//$names="licangcai,qiukh"
-function buddy($names, $uids = null) {
-	global $_SGLOBAL,$user, $groups;
-	$where_name = "";
-	$where_uid = "";
-	if(!$names and !$uids)return array();
-	if($names){
-		$names = "'".implode("','", explode(",", $names))."'";
-		$where_name = "m.username IN ($names)";
-	}
-	if($uids){
-		$where_uid = "m.uid IN ($uids)";
-	}
-	$where_sql = $where_name && $where_uid ? "($where_name OR $where_uid)" : ($where_name ? $where_name : $where_uid);
-	$buddies = array();
-	$query = $_SGLOBAL['db']-> query($q="SELECT m.uid, m.username, m.name, f.gid, f.fuid 
-		FROM " .tname('space')." m 
-		LEFT OUTER JOIN ".tname('friend')." f ON f.uid = '$user->uid' AND m.uid = f.fuid 
-		WHERE m.uid <> $user->uid AND $where_sql");
-	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-		if(empty($value['fuid'])) {
-			$group = "stranger";
-		}else {
-			$gid = $value['gid'];
-			$group = (empty($gid) || empty($groups[$gid])) ? "friend" : $groups[$gid];
-		}
-		$buddies[]=(object)array(
-			'uid'=>$value['uid'],
-			'id'=> $value['username'],
-			'nick'=> nick($value),
-			'pic_url' =>avatar($value['uid'],"small",true),
-			'status'=>'' ,
-			'status_time'=>'',
-			'url'=>'space.php?uid='.$value['uid'],
-			'group'=> $group,
-			'default_pic_url' => UC_API.'/images/noavatar_small.gif');
-	}
-	return $buddies;
-}
-
-function rooms() {
-	global $_SGLOBAL,$user;
-	$rooms = array();
-	$query = $_SGLOBAL['db']->query("SELECT t.tagid, t.membernum, t.tagname, t.pic
-		FROM ".tname('tagspace')." main
-		LEFT JOIN ".tname('mtag')." t ON t.tagid = main.tagid
-		WHERE main.uid = '$user->uid'");
-	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-		$tagid = $value['tagid'];
-		$id = $tagid;
-		$tagname = $value['tagname'];
-		$pic = empty($value['pic']) ? 'image/nologo.jpg' : $value['pic'];
-		$rooms[$id]=(object)array('id'=>$id,
-			'nick'=> $tagname,
-			'pic_url'=>$pic,
-			'status'=>'',
-			'status_time'=>'',
-			'all_count' => $value['membernum'],
-			'url'=>'space.php?do=mtag&tagid='.$tagid,
-			'count'=>"");
-	}
-	return $rooms;
-}
-
-function new_message_to_histroy() {
-	global $user, $ucdb;
-	$id = $user->id;
-	$ucdb->query("UPDATE ".im_tname('histories')." SET send = 1 WHERE `to`='$id' AND send = 0");
-}
 
 /**
- * Get history message
- *
- * @param string $type unicast or multicast
- * @param string $id
- *
- * Example:
- * 	history('unicast', 'webim');
- * 	history('multicast', '36');
  *
  */
 
-function history($type, $id){
-	global $user, $ucdb;
-	$user_id = $user->id;
-	$list = array();
-	if($type == "unicast"){
-		$query = $ucdb->query("SELECT * FROM ".im_tname('histories')." 
-			WHERE `type` = 'unicast' 
-			AND ((`to`='$id' AND `from`='$user_id' AND `fromdel` != 1) 
-			OR (`send` = 1 AND `from`='$id' AND `to`='$user_id' AND `todel` != 1))  
-			ORDER BY timestamp DESC LIMIT 30");
-		while ($value = $ucdb->fetch_array($query)){
-			array_unshift($list, log_item($value));
-		}
-	}elseif($type == "multicast"){
-		$query = $ucdb->query("SELECT * FROM ".im_tname('histories')." 
-			WHERE `to`='$id' AND `type`='multicast' AND send = 1 
-			ORDER BY timestamp DESC LIMIT 30");
-		while ($value = $ucdb->fetch_array($query)){
-			array_unshift($list, log_item($value));
-		}
-	}else{
-	}
-	return $list;
+// Die if PHP is not new enough
+if ( version_compare( PHP_VERSION, '4.3', '<' ) ) {
+	die( sprintf( 'Your server is running PHP version %s but webim requires at least 4.3', PHP_VERSION ) );
 }
 
-/**
- * Get new message
+if ( !defined( 'WEBIM_PATH' ) ) 
+	define( 'WEBIM_PATH', dirname( __FILE__ ) . '/' );
+
+if ( !defined( 'WEBIMDB_DEBUG' ) )
+	define( 'WEBIMDB_DEBUG', true );
+
+if ( !defined( 'WEBIMDB_CHARSET' ) )
+	define( 'WEBIMDB_CHARSET', 'utf8' );
+
+// Modify error reporting levels to exclude PHP notices
+error_reporting( E_ALL ^ E_NOTICE );
+
+require_once( WEBIM_PATH . 'lib/functions.helper.php' );
+require_once( WEBIM_PATH . 'lib/functions.json.php' );
+require_once( WEBIM_PATH . 'lib/functions.actions.php' );
+require_once( WEBIM_PATH . 'lib/class.webim_db.php' );
+
+require_once( WEBIM_PATH . 'lib/http_client.php' );
+require_once( WEBIM_PATH . 'lib/class.webim_client.php' );
+
+/** 
+ * Custom interface 
+ *
+ * Provide 
+ *
+ * array $_IMC
+ * boolean $im_is_admin
+ * boolean $im_is_login
+ * object $imuser require when $im_is_login is true
+ * function webim_get_buddies( $ids )
+ * function webim_get_menu() require when !$_IMC['disable_menu']
+ * function webim_get_online_buddies()
+ * function webim_get_rooms( $ids )
+ * function webim_get_notifications()
+ * function webim_login( $username, $password, $question, $answer ) require when $_IMC['allow_login'] is true
  *
  */
 
-function new_message() {
-	global $user, $ucdb;
-	$id = $user->id;
-	$list = array();
-	$query = $ucdb->query("SELECT * FROM ".im_tname('histories')." 
-		WHERE `to`='$id' and send = 0 
-		ORDER BY timestamp DESC LIMIT 100");
-	while ($value = $ucdb->fetch_array($query)){
-		array_unshift($list, log_item($value));
-	}
-	return $list;
-}
+require_once( WEBIM_PATH . 'interface.php' );
 
-function log_item($value){
-	return (object)array(
-		'to' => $value['to'],
-		'nick' => $value['nick'],
-		'from' => $value['from'],
-		'style' => $value['style'],
-		'body' => $value['body'],
-		'type' => $value['type'],
-		'timestamp' => $value['timestamp']
-	);
-}
+/**
+ * $im_params = array_merge( $_GET, $_POST );
+ */
 
-function setting() {
-	global $_SGLOBAL,$user, $ucdb;
-	if(!empty($_SGLOBAL['supe_uid'])) {
-		$setting  = $ucdb->fetch_array($ucdb->query("SELECT * FROM ".im_tname('settings')." WHERE uid='$_SGLOBAL[supe_uid]'"));
-		if(empty($setting)) {
-			$setting = array('uid'=> $user->uid,'web'=>"");
-			$ucdb->query("INSERT INTO ".im_tname('settings')." (uid,web) VALUES ($_SGLOBAL[supe_uid],'')");
-		}
-		$setting = $setting["web"];
+/** $imdb, $imuser, $imclient, $_IMC */
+$imdb = new webim_db( $_IMC['dbuser'], $_IMC['dbpassword'], $_IMC['dbname'], $_IMC['dbhost'] );
+$imdb->set_prefix( $_IMC['dbtable_prefix'] );
+$imdb->add_tables( array( 'webim_settings', 'webim_histories' ) );
+if ( $im_is_login ) {
+	$imticket = webim_gp( 'ticket' );
+	if( $imticket ) {
+		$imticket = stripslashes($imticket);
 	}
-	return json_decode(empty($setting) ? "{}" : $setting);
-}
-
-function to_utf8($s) {
-	global $_SC;
-	if($_SC['charset'] == 'utf-8') {
-		return $s;
-	} else {
-		return  _iconv($_SC['charset'],'utf-8',$s);
-	}
-}
-
-function from_utf8($s) {
-	global $_SC;
-	if($_SC['charset'] == 'utf-8') {
-		return $s;
-	} else {
-		return  _iconv('utf-8',$_SC['charset'],$s);
-	}
+	$imclient = new webim_client( $imuser, $imticket, $_IMC['domain'], $_IMC['apikey'], $_IMC['host'], $_IMC['port'] );
+	unset( $imticket );
 }
 
 ?>
---
+++ interface.php
@@ -0,0 +1,316 @@
+<?php
+
+/** 
+ * Custom interface 
+ *
+ * Provide 
+ *
+ * array $_IMC
+ * boolean $im_is_admin
+ * boolean $im_is_login
+ * object $imuser require when $im_is_login
+ * function webim_get_menu() require when !$_IMC['disable_menu']
+ * function webim_get_buddies()
+ * function webim_get_online_buddies()
+ * function webim_get_rooms()
+ * function webim_get_notifications()
+ * function webim_login()
+ *
+ */
+
+require_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'common.php');
+
+//Find and insert data with utf8 client.
+$_SGLOBAL['db']->query("SET NAMES utf8");
+
+require 'config.php';
+
+/**
+ *
+ * Provide the webim database config.
+ *
+ * $_IMC['dbuser'] MySQL database user
+ * $_IMC['dbpassword'] MySQL database password
+ * $_IMC['dbname'] MySQL database name
+ * $_IMC['dbhost'] MySQL database host
+ * $_IMC['dbtable_prefix'] MySQL database table prefix
+ * $_IMC['dbcharset'] MySQL database charset
+ *
+ */
+
+$_IMC['dbuser'] = UC_DBUSER;
+$_IMC['dbpassword'] = UC_DBPW;
+$_IMC['dbname'] = UC_DBNAME;
+$_IMC['dbhost'] = UC_DBHOST;
+$prefix = explode(".", UC_DBTABLEPRE );
+$_IMC['dbtable_prefix'] = count( $prefix ) == 2 ? $prefix[1] : $prefix[0];
+$_IMC['dbcharset'] = UC_DBCHARSET;
+
+/**
+ * Init im user.
+ * 	-uid:
+ * 	-id:
+ * 	-nick:
+ * 	-pic_url:
+ * 	-show:
+ *
+ */
+
+if(empty($_SGLOBAL['supe_uid'])) {
+	$im_is_login = false;
+} else {
+	$im_is_login = true;
+	$space = getspace($_SGLOBAL['supe_uid']);
+	webim_set_user();
+}
+
+function webim_set_user(){
+	global $space, $imuser;
+	$imuser->uid = $space['uid'];
+	$imuser->id = $space['username'];
+	$imuser->nick = nick($space);
+	$imuser->pic_url = avatar($imuser->uid,"small",true);
+	$imuser->default_pic_url=UC_API.'/images/noavatar_small.gif';
+	$imuser->show = webim_gp('show') ? webim_gp('show') : "available";
+	$imuser->url = "space.php?uid=".$imuser->uid;
+	complete_status( array( $imuser ) );
+}
+
+function webim_login( $username, $password, $question = "", $answer = "" ) {
+	global $imuser, $_G, $im_is_login;
+	return false;
+}
+
+//Cache friend_groups;
+$friend_groups = getfriendgroup();
+foreach($friend_groups as $k => $v){
+	$friend_groups[$k] = to_utf8($v);
+}
+
+/**
+ * Online buddy list.
+ *
+ */
+function webim_get_online_buddies(){
+	global $friend_groups, $imuser, $_SGLOBAL;
+	$list = array();
+	$query = $_SGLOBAL['db']->query("SELECT m.uid, m.username, m.name, f.gid 
+		FROM ".tname('friend')." f, ".tname('session')." s, ".tname('space')." m
+		WHERE f.uid='$imuser->uid' AND f.fuid = s.uid AND m.uid = s.uid
+		ORDER BY f.num DESC, f.dateline DESC");
+	while ($value = $_SGLOBAL['db']->fetch_array($query)){
+		$list[] = (object)array(
+			"uid" => $value['uid'],
+			"id" => $value['username'],
+			"nick" => nick($value),
+			"group" => $friend_groups[$value['gid']],
+			"url" => "home.php?mod=space&uid=".$value['uid'],
+			'default_pic_url' => UC_API.'/images/noavatar_small.gif',
+			"pic_url" => avatar($value['uid'], 'small', true),
+		);
+	}
+	complete_status( $list );
+	return $list;
+}
+
+/**
+ * Get buddy list from given ids
+ * $ids:
+ *
+ * Example:
+ * 	buddy('admin,webim,test');
+ *
+ */
+
+function webim_get_buddies( $names, $uids = null ){
+	global $_SGLOBAL, $imuser, $friend_groups;
+	$where_name = "";
+	$where_uid = "";
+	if(!$names and !$uids)return array();
+	if($names){
+		$names = "'".implode("','", explode(",", $names))."'";
+		$where_name = "m.username IN ($names)";
+	}
+	if($uids){
+		$where_uid = "m.uid IN ($uids)";
+	}
+	$where_sql = $where_name && $where_uid ? "($where_name OR $where_uid)" : ($where_name ? $where_name : $where_uid);
+	$list = array();
+	$query = $_SGLOBAL['db']-> query("SELECT m.uid, m.username, m.name, f.gid, f.fuid 
+		FROM " .tname('space')." m 
+		LEFT OUTER JOIN ".tname('friend')." f ON f.uid = '$imuser->uid' AND m.uid = f.fuid 
+		WHERE m.uid <> $imuser->uid AND $where_sql");
+	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+		if(empty($value['fuid'])) {
+			$group = "stranger";
+		}else {
+			$gid = $value['gid'];
+			$group = (empty($gid) || empty($friend_groups[$gid])) ? "friend" : $friend_groups[$gid];
+		}
+		$list[]=(object)array(
+			'uid'=>$value['uid'],
+			'id'=> $value['username'],
+			'nick'=> nick($value),
+			'pic_url' =>avatar($value['uid'],"small",true),
+			'status'=>'' ,
+			'status_time'=>'',
+			'url'=>'space.php?uid='.$value['uid'],
+			'group'=> $group,
+			'default_pic_url' => UC_API.'/images/noavatar_small.gif');
+	}
+	complete_status( $list );
+	return $list;
+}
+
+/**
+ * Get room list
+ * $ids: Get all imuser rooms if not given.
+ *
+ */
+
+function webim_get_rooms($ids=null){
+	global $_SGLOBAL,$imuser;
+	$rooms = array();
+	$query = $_SGLOBAL['db']->query("SELECT t.tagid, t.membernum, t.tagname, t.pic
+		FROM ".tname('tagspace')." main
+		LEFT JOIN ".tname('mtag')." t ON t.tagid = main.tagid
+		WHERE main.uid = '$imuser->uid'");
+	while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+		$tagid = $value['tagid'];
+		$id = $tagid;
+		$tagname = $value['tagname'];
+		$pic = empty($value['pic']) ? 'image/nologo.jpg' : $value['pic'];
+		$rooms[$id]=(object)array('id'=>$id,
+			'nick'=> $tagname,
+			'pic_url'=>$pic,
+			'status'=>'',
+			'status_time'=>'',
+			'all_count' => $value['membernum'],
+			'url'=>'space.php?do=mtag&tagid='.$tagid,
+			'count'=>"");
+	}
+	return $rooms;
+}
+
+function webim_get_notifications(){
+	global $_SGLOBAL;
+	include_once S_ROOT.'./uc_client/client.php';
+	$pmlist = array();
+	$member = $_SGLOBAL['member'];
+	if($member['notenum']){
+		$pmlist[]=array("text"=>('<img src="image/icon/notice.gif" width="16" alt="" /><strong>'.$member["notenum"].'</strong> 个新通知'),"link"=>"space.php?do=notice");
+	}
+	if($member['pokenum']){
+		$pmlist[]=array("text"=>('<img src="image/icon/poke.gif" width="16" alt="" /><strong>'.$member["pokenum"].'</strong> 个新招呼'),"link"=>"cp.php?ac=poke");
+	}
+	if($member['addfriendnum']){
+		$pmlist[]=array("text"=>('<img src="image/icon/friend.gif" width="16" alt="" /><strong>'.$member["addfriendnum"].'</strong> 个好友请求'),"link"=>"cp.php?ac=friend&op=request");
+	}
+	if($member['mtaginvitenum']){
+		$pmlist[]=array("text"=>('<img src="image/icon/mtag.gif" width="16" alt="" /><strong>'.$member["mtaginvitenum"].'</strong> 个群组邀请'),"link"=>"cp.php?ac=mtag&op=mtaginvite");
+	}
+	if($member['eventinvitenum']){
+		$pmlist[]=array("text"=>('<img src="image/icon/event.gif" width="16" alt="" /><strong>'.$member["eventinvitenum"].'</strong> 个活动邀请'),"link"=>"cp.php?ac=event&op=eventinvite");
+	}
+	if($member['myinvitenum']){
+		$pmlist[]=array("text"=>('<img src="image/icon/userapp.gif" width="16" alt="" /><strong>'.$member["myinvitenum"].'</strong> 个应用消息'),"link"=>"space.php?do=notice&view=userapp");
+	}
+
+	$pmstatus = uc_pm_checknew($user->uid, 0);
+	$filter =  'newpm';
+	$ucdata = uc_pm_list($user->uid, 1, 20, "inbox", "newpm", 150);
+
+	foreach($ucdata['data'] as $pm) {
+		if ($pm['msgfromid'] > 0) {
+			$from=$pm['msgfrom'];
+			$text=$pm['msgfrom']." 给你发了一条消息！";
+			$link= 'space.php?do=pm&filter=newpm&uid='.$pm['touid'].'&filter=newpm&daterange='.$pm['daterange'];
+		}else {
+			$from='';
+			$text="系统信息：".$pm['subject'];
+			$link= 'space.php?do=pm&filter=newpm?pmid='.$pm['pmid'].'&filter=systempm';
+		}
+		$pmlist[]= array('from'=>$from,'text'=>$text,'link'=>$link,'time'=>$pm['dateline']);
+	}
+	return $pmlist;
+}
+
+function webim_get_menu() {
+	global $_SCONFIG, $_SGLOBAL;
+	$menu = array(
+		array("title" => 'doing',"icon" =>"image/app/doing.gif","link" => "space.php?do=doing"),
+		array("title" => 'album',"icon" =>"image/app/album.gif","link" => "space.php?do=album"),
+		array("title" => 'blog',"icon" =>"image/app/blog.gif","link" => "space.php?do=blog"),
+		array("title" => 'thread',"icon" =>"image/app/mtag.gif","link" => "space.php?do=thread"),
+		array("title" => 'share',"icon" =>"image/app/share.gif","link" => "space.php?do=share")
+	);
+
+	if($_SCONFIG['my_status']) {
+		if(is_array($_SGLOBAL['userapp'])) { 
+			foreach($_SGLOBAL['userapp'] as $value) { 
+				$menu[] = array("title" => to_utf8($value['appname']), "icon" =>"http://appicon.manyou.com/icons/".$value['appid'],"link" => "userapp.php?id=".$value['appid']);
+			}
+		}
+		if(is_array($_SGLOBAL['my_menu'])) { 
+			foreach($_SGLOBAL['my_menu'] as $value) { 
+				$menu[] = array("title" => to_utf8($value['appname']), "icon" =>"http://appicon.manyou.com/icons/".$value['appid'],"link" => "userapp.php?id=".$value['appid']);
+			}
+		}
+	}
+	return $menu;
+}
+
+/**
+ * Add status to member info.
+ *
+ * @param array $members the member list
+ * @return 
+ *
+ */
+function complete_status( $members ) {
+	global $_SGLOBAL;
+	if(!empty($members)){                
+		$num = count($members);                
+		$ids = array();
+		$ob = array();
+		for($i = 0; $i < $num; $i++){
+			$m = $members[$i];
+			$id = $m->uid;
+			if ( $id ) {
+				$ids[] = $id;
+				$ob[$id] = $m;
+			}
+		}
+		$ids = implode(",", $ids);
+		$query = $_SGLOBAL['db']-> query($q="SELECT t.uid, t.message FROM " . tname("doing") . " t 
+			INNER JOIN (SELECT max(doid) doid 
+			FROM " . tname("doing") . "  
+			WHERE uid IN ($ids)
+			GROUP BY uid) t2 
+			ON t2.doid = t.doid;");
+		while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+			$ob[$value['uid']]->status = $value['message'];
+		}
+	}
+	return $members;
+}
+
+function nick( $sp ) {
+	global $_IMC;
+	return (!$_IMC['show_realname']||empty($sp['name'])) ? $sp['username'] : $sp['name'];
+}
+
+function to_utf8( $s ) {
+	if( strtoupper( CHARSET ) == 'UTF-8' ) {
+		return $s;
+	} else {
+		if ( function_exists( 'iconv' ) ) {
+			return iconv( CHARSET, 'utf-8', $s );
+		} else {
+			require_once 'class_chinese.php';
+			$chs = new Chinese( CHARSET, 'utf-8' );
+			return $chs->Convert( $s );
+		}
+	}
+}
+
