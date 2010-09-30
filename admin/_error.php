7dca2099287326ab455be9078427f7a8113933a8
Hidden <zzdhidden@gmail.com>
2010-09-30 11:07:17 +0800

    Format

--- admin/_error.php
+++ admin/_error.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>WebIM</title>
-		<link href="base.css" media="all" type="text/css" rel="stylesheet" />
-	</head>
-	<body>
-		<h1>WebIM</h1>
-		<div id="wrap">
-		<?php echo $msg ?>
-		</div>
-		<div id="footer"><p><a href="http://www.nextim.cn">© 2010 NextIM</a></p></div>
-	</body>
-</html>
+<?php
+
+require_once( dirname( __FILE__ ) . '/' . 'admin_common.php' );
+
+echo webim_header( '' );
+
+echo $msg;
+
+echo webim_footer();
+
+?>
 
