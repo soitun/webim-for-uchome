0da341f3e9f6d6f6c02cd3aaa378ef3eb61f9c54
Hidden <zzdhidden@gmail.com>
2010-09-30 11:15:56 +0800

    Html format

--- admin/check.php
+++ admin/check.php
@@ -1,20 +1,31 @@
 <?php
 
-header("Content-type: application/javascript");
 require_once( dirname( __FILE__ ) . '/' . 'admin_common.php' );
-echo "IM Config\n-------------------------\n";
-echo "domain: '" . $_IMC['domain'] . "'\n";
-echo "host: '" . $_IMC['host'] . "'\n";
-echo "port: '" . $_IMC['port'] . "'\n";
+$html =  "<h3>IM Config</h3>";
+$html .= "<p>domain: '" . $_IMC['domain'] . "'</p>";
+$html .= "<p>host: '" . $_IMC['host'] . "'</p>";
+$html .= "<p>port: '" . $_IMC['port'] . "'</p>";
 
-echo "\n\nAllow url fopen\n-------------------------\n";
-echo ini_get('allow_url_fopen') ? "On" : "Off";
+$html .= "<h3>Allow url fopen</h3>";
+$html .= ini_get('allow_url_fopen') ? "On" : "Off";
 
-echo "\n\nIM check online\n-------------------------\n";
+$html .= "<h3>IM check online</h3>";
 $data = $imclient->check_connect();
-echo $data->success ? "Success" : ( "Faild: " . $data->error_msg );
+$html .= $data->success ? "Success" : ( "Faild: " . $data->error_msg );
 
-echo "\n\nTest for port 80\n-------------------------\n";
+$html .= "<h3>Test for port 80</h3>";
 $content = file_get_contents("http://www.google.com");
-echo empty( $content ) ? "Faild" : "Success";
+$html .= empty( $content ) ? "Faild" : "Success";
+
+require_once( dirname( __FILE__ ) . '/' . 'admin_common.php' );
+
+echo webim_header( '' );
+
+echo webim_menu( 'check' );
+
+echo '<div id="content">';
+echo $html;
+echo '</div>';
+
+echo webim_footer();
 
